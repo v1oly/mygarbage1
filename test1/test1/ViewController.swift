d33df69aeac36e44786956db649f36982d89448f
violy42 <violythegod@gmail.com>
2021-09-01 17:46:35 +0300

    Create test1 project

---
+++ test1/test1/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  test1
+//
+//  Created by Марк Некрашевич on 31.08.2021.
+//  Copyright © 2021 Mark Nekrashevich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var flipCount = 0 {
+        didSet {
+          totalFlips.text = "Flips: \(flipCount)"
+        }
+    }
+    
+    @IBOutlet var emojiArray: [UIButton]!
+    
+    @IBOutlet weak var totalFlips: UILabel!
+    
+    @IBAction func mainButtonFunc(_ sender: UIButton) {
+        flipButton(withEmoji: "😎", on: sender)
+        flipCount += 1
+        var cardNumber = emojiArray.index(of: <#T##UIButton#>)
+    }
+    
+   
+    
+    func flipButton(withEmoji emoji: String, on button: UIButton){
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
+        } else {
+                button.setTitle(emoji, for: UIControlState.normal)
+                button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+            
+        }
+    }
+}
+
