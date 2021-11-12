35cfd688ea49aa276c5bd52999d2adf81b49a4d9
Mark Nekrashevich <violythegod@gmail.com>
2021-11-12 22:06:15 +0300

    dz5 12/11

---
+++ dz/dz/DetailsView.swift
@@ -0,0 +1,9 @@
+//
+//  DetailsView.swift
+//  dz
+//
+//  Created by Markus on 12.11.2021.
+//  Copyright © 2021 Mark Nekrashevich. All rights reserved.
+//
+
+import Foundation
---
+++ dz/dz/DetailsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  DetailsViewController.swift
+//  dz
+//
+//  Created by Markus on 12.11.2021.
+//  Copyright © 2021 Mark Nekrashevich. All rights reserved.
+//
+
+import Foundation
