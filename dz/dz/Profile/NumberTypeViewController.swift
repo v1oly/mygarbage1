76cc363e739ee03e5553526efb37500f3cf05f80
Mark Nekrashevich <violythegod@gmail.com>
2021-12-15 10:54:35 +0300

    dz 5 probably final ( ͡° ͜ʖ ͡°)

--- dz/dz/Profile/NumberTypeViewController.swift
+++ dz/dz/Profile/NumberTypeViewController.swift
@@ -1,11 +1,13 @@
 import UIKit
 
-class NumberTypeField: UITextField, UITextFieldDelegate {
+class NumberTypeField: UITextField {
+    var typedNumbers = ""
+    var typedNumbersBuffer = 0
+    var positions: [Int] = []
+    var deletePositions: [Int] = []
+    var inc = true
+    var currentIndex = 0
     
-    var count = false
-    var textBuffer = ""
-    var numberCountBuffer = 0
-
     override init(frame: CGRect) {
         super.init(frame: frame)
         setup()
@@ -17,10 +19,7 @@ class NumberTypeField: UITextField, UITextFieldDelegate {
     }
     
     func setup() {
-        let startPosition: UITextPosition = self.beginningOfDocument
         let placeholderText = "+X(XXX) XXX XX-XX"
-        let attributes = [NSAttributedString.Key.foregroundColor: UIColor.darkGray]
-        let attributedPlaceholder = NSAttributedString(string: placeholderText, attributes: attributes)
         
         self.addTarget(self, action: #selector(self.textFieldDidChange(_:)), for: .editingChanged)
         self.frame.size = CGSize(
@@ -34,7 +33,11 @@ class NumberTypeField: UITextField, UITextFieldDelegate {
         self.backgroundColor = .lightGray
         self.borderStyle = .roundedRect
         self.textAlignment = .center
-        self.attributedPlaceholder = attributedPlaceholder
+        self.font = UIFont.systemFont(ofSize: 20.0)
+        self.placeholder = placeholderText
+        
+        positions = getNumPositions(mask: "+X(XXX) XXX XX-XX")
+        deletePositions = positions.map { $0 + 1 }
     }
     
     func replace(myString: String, _ index: Int, _ newChar: Character) -> String {
@@ -42,6 +45,117 @@ class NumberTypeField: UITextField, UITextFieldDelegate {
         chars[index] = newChar
         let modifiedString = String(chars)
         return modifiedString
+    }
+    
+    func getNumPositions(mask: String) -> [Int] {
+        var arr: [Int] = []
+        for (index, char) in mask.enumerated() {
+            if char == "X" {
+                arr += [index]
+            }
+        }
+        return arr
+    }
+    
+    func floatingCursorSetPosition(isInc: Bool) {
+        if isInc == true {
+            currentIndex = tpToClosestNumber(string: self.text ?? "")
+            print(currentIndex)
+            if currentIndex < positions.count && typedNumbers.count < 11 {
+                if let newPos = self.position(from: self.beginningOfDocument, offset: +positions[currentIndex]) {
+                    self.selectedTextRange = self.textRange(from: newPos, to: newPos)
+                }
+            }
+        }
+        
+        if isInc == false {
+            currentIndex = tpToClosestDeleteNumber(string: self.text ?? "")
+            print(" delete index - \(currentIndex)")
+            if currentIndex >= 0 && !typedNumbers.isEmpty {
+                if let newPos = self.position(from: self.beginningOfDocument, offset: +deletePositions[currentIndex]) {
+                    self.selectedTextRange = self.textRange(from: newPos, to: newPos)
+                }
+            }
+        }
+        if typedNumbers.isEmpty {
+            if let newPos = self.position(from: self.beginningOfDocument, offset: +1) {
+                self.selectedTextRange = self.textRange(from: newPos, to: newPos)
+            }
+        }
+        if typedNumbers.count == positions.count {
+            UIApplication.shared.sendAction(#selector(self.resignFirstResponder), to: nil, from: nil, for: nil)
+        }
+    }
+    
+    func tpToClosestNumber(string: String) -> Int {
+        var nextIndex = 0
+        var oneTime = true
+        let arr = Array(string)
+        for index in arr.indices {
+            if arr[index] == "X" && oneTime {
+                nextIndex = index
+                oneTime = !oneTime
+            }
+        }
+        for index in positions.indices {
+            if positions[index] == nextIndex {
+                nextIndex = index
+            }
+        }
+        return nextIndex
+    }
+    
+    func tpToClosestDeleteNumber(string: String) -> Int {
+        var nextIndex = 0
+        let arr = Array(string)
+        for index in arr.indices {
+            if arr[index] == typedNumbers.last {
+                nextIndex = index + 1
+            }
+        }
+        for index in deletePositions.indices {
+            if deletePositions[index] == nextIndex {
+                nextIndex = index
+            }
+        }
+        return nextIndex
+    }
+    
+    func placeholderTextVision(string: String) -> NSAttributedString? {
+        var lastNumberIndex = 0
+        var arr = Array(string)
+        for index in arr.indices {
+            if arr[index].isNumber {
+                lastNumberIndex = index
+            }
+        }
+        
+        if lastNumberIndex == 0 {
+            let str1 = String(arr)
+            let attributes1 = [NSAttributedString.Key.foregroundColor: UIColor.darkGray]
+            let attributedText1 = NSMutableAttributedString(string: str1, attributes: attributes1)
+            return attributedText1
+        } else if lastNumberIndex == positions.last {
+            let str1 = String(arr)
+            let attributes1 = [NSAttributedString.Key.foregroundColor: UIColor.black]
+            let attributedText1 = NSMutableAttributedString(string: str1, attributes: attributes1)
+            return attributedText1
+        } else {
+            arr.insert("^", at: lastNumberIndex + 1)
+            let separatedString = String(arr).split(separator: "^")
+            
+            let str1 = String(separatedString[0])
+            let str2 = String(separatedString[1])
+            
+            let attributes1 = [NSAttributedString.Key.foregroundColor: UIColor.black]
+            let attributes2 = [NSAttributedString.Key.foregroundColor: UIColor.darkGray]
+            
+            let attributedText1 = NSMutableAttributedString(string: str1, attributes: attributes1)
+            let attributedText2 = NSMutableAttributedString(string: str2, attributes: attributes2)
+            
+            attributedText1.append(attributedText2)
+            return attributedText1
+        }
     }
     
     func typePhoneNumber(mask: String, phone: String) -> String {
@@ -67,71 +181,29 @@ class NumberTypeField: UITextField, UITextFieldDelegate {
         guard let phoneString = self.text else {
             return
         }
-        self.text = typePhoneNumber(mask: "+X(XXX) XXX XX-XX", phone: phoneString)
+        let string = typePhoneNumber(mask: "+X(XXX) XXX XX-XX", phone: phoneString)
         
-//        var numberCount = 0
-//        let arrayPhoneString = Array(phoneString)
-//        for char in arrayPhoneString {
-//            switch char {
-//            case "1":
-//                numberCount += 1
-//            case "2":
-//                numberCount += 1
-//            case "3":
-//                numberCount += 1
-//            case "4":
-//                numberCount += 1
-//            case "5":
-//                numberCount += 1
-//            case "6":
-//                numberCount += 1
-//            case "7":
-//                numberCount += 1
-//            case "8":
-//                numberCount += 1
-//            case "9":
-//                numberCount += 1
-//            case "0":
-//                numberCount += 1
-//            default:()
-//            }
-//        }
-//
-//        if !count {
-//            let newPosition = textField.beginningOfDocument
-//            textField.selectedTextRange = textField.textRange(from: newPosition, to: newPosition)
-//            count = true
-//        }
-//        if count && (numberCountBuffer > numberCount) {
-//            if let selectedRange = self.selectedTextRange {
-//
-//                // and only if the new position is valid
-//                if let newPosition = self.position(from: selectedRange.start, offset: -1) {
-//
-//                    // set the new position
-//                    self.selectedTextRange = self.textRange(from: newPosition, to: newPosition)
-//                }
-//            }
-//        }
-//        if count && (numberCountBuffer < numberCount) {
-//            if let selectedRange = self.selectedTextRange {
-//
-//                // and only if the new position is valid
-//                if let newPosition = self.position(from: selectedRange.start, offset: +1) {
-//
-//                    // set the new position
-//                    self.selectedTextRange = self.textRange(from: newPosition, to: newPosition)
-//                }
-//            }
-//        }
-//        numberCountBuffer = numberCount
-//        print("number count \(numberCount)")
-//        print("number count buffer \(numberCountBuffer)")
+        self.attributedText = placeholderTextVision(string: string)
+        
+        let countTypedNumbersBuffer = typedNumbers.count
+        typedNumbers = ""
+        for char in phoneString where typedNumbers.count < 11 {
+            if char.wholeNumberValue != nil {
+                typedNumbers += String(char)
+            }
+        }
+        
+        if typedNumbers.count > countTypedNumbersBuffer {
+            inc = true
+        } else if typedNumbers.count < countTypedNumbersBuffer {
+            inc = false
+        }
+        floatingCursorSetPosition(isInc: inc)
     }
 }
 
 class NumberTypeViewController: ViewController {
-   
+    
     let numberTypeField = NumberTypeField()
     
     override func viewDidLoad() {
